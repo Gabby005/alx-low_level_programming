d1fd31bfd3341f0d7fef36c32b9c001a0dcfc080
Gabby005 <olamide_okunoye@yahoo.com>
2022-09-08 15:56:09 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -12,7 +12,8 @@
 
 	{
 
-		printf("with proper grammar, but the outcome is a piece of art,\n");
+		printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+		
 		return (0);
 	
 	}
