6c6e1826443aacd8a7fd7f91ba354e1b7da93c4c
Gabby005 <olamide_okunoye@yahoo.com>
2022-09-16 05:15:30 -0700

    Write a program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - utilizes on the _putchar function to print
+ * the alphabet a - z
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char ch;
+	
+	for (ch = 'a'; <= 'z'; ++ch)
+	{
+		_putchar(ch);
+	}
+_putchar('\n');
+return (0);
+
+}
