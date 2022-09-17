d1efa5b80a04d59d310924c1b4a36f677b016b3d
Gabby005 <olamide_okunoye@yahoo.com>
2022-09-17 11:47:24 -0700

    The “Fizz-Buzz test” is an interview question designed to help filter out the 99.5% of programming job candidates who can’t seem to program their way out of a wet paper bag

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,14 +2,14 @@
 
 /**
  * main - function that checks for uppercase character.
- *
  * Return: 0
  */
 
 int main(void)
 {
 	int i = 1;
-	
+
+
 	while (i <= 100)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
