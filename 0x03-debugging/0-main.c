0e838c100d81b7192a08aa6decbaac070e0672f2
Gabby005 <olamide_okunoye@yahoo.com>
2022-09-16 19:33:26 -0700

    Based on the main.c file above, create a file named 0-main.c. This file must test that the function positive_or_negative() gives the correct output when given a case of 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
+ * main - tests function that prints if integer is + or -
  * Return: 0
  */
 
@@ -13,4 +13,5 @@ i = 0;
 positive_or_negative(i);
 	
 return (0);
+
 }
