5ecd6674e6b2b5c708ccdf188c589dd1bd008f4c
Gabby005 <olamide_okunoye@yahoo.com>
2023-04-14 09:16:11 -0700

    Added Test Files

---
+++ 0x1E-search_algorithms/tests/102-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../search_algos.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+int main(void)
+{
+    int array[] = {
+        0, 0, 1, 2, 2, 2, 2, 3, 3, 4, 4, 5, 6, 6, 7, 8, 8, 8, 9, 9
+
